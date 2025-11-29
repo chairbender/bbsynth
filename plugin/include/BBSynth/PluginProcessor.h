2a5c1ba4d36ad3a5ac9f936ee137a1b177f574b1
chairbender <chairbender@gmail.com>
2025-11-28 21:03:32 -0800

    wip implementing hello world synth

--- plugin/include/BBSynth/PluginProcessor.h
+++ plugin/include/BBSynth/PluginProcessor.h
@@ -36,6 +36,8 @@ public:
   void setStateInformation(const void* data, int sizeInBytes) override;
 
 private:
+  SynthAudioSource synthAudioSource;
+
   JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(AudioPluginAudioProcessor)
 };
 }  // namespace audio_plugin
