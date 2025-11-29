5cf7d5a18fd3374a039783a53f10f7c9ff5cb79c
chairbender <chairbender@gmail.com>
2025-11-29 11:33:19 -0800

    map centoffset to slider and fix rendering issue

--- plugin/include/BBSynth/PluginEditor.h
+++ plugin/include/BBSynth/PluginEditor.h
@@ -16,7 +16,9 @@ private:
   // This reference is provided as a quick way for your editor to
   // access the processor object that created it.
   AudioPluginAudioProcessor& processorRef;
-  juce::Slider tailOffSlider;
+  juce::Label centOffsetLabel;
+  juce::Slider centOffsetSlider;
+  std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> centOffsetAttachment;
 
   JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(AudioPluginAudioProcessorEditor)
 };
