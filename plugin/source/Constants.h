831868f2f98ebebb3ad19af91c5433b7bfda57c9
chairbender <kwhipke1@gmail.com>
2026-01-21 23:10:34 -0800

    wip fixing MPE crackling

--- plugin/source/Constants.h
+++ plugin/source/Constants.h
@@ -3,14 +3,14 @@
 #include <ranges>
 
 namespace audio_plugin {
-constexpr auto kOversample = 2;
+constexpr auto kOversample = 1;
 // at drive slider of "0" we still want SOME drive - the "natural" drive of the OTA.
 // Having 0 actual drive creates instability;
 constexpr auto kMinDrive = .5f;
 constexpr auto kMinCutoff = 20.0f;
 // todo: I think it doesn't serve much purpose to allow it to go higher than the nyquist freq?
 constexpr auto kMaxCutoff = 22000.0f;
-constexpr auto kNumVoices = 4;
+constexpr auto kNumVoices = 1;
 
 // todo: not sure if putting a function here is a best practice...
 constexpr auto GetIndexSuffixedParams(const std::string_view param_prefix) {
